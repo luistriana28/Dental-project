0a8bc9568ff6ccd54fc543a3cd97effb5c7977da
Luis Triana <luis.triana@jarsa.com.mx>
2017-07-10 20:48:58 -0500

    [ADD]-Database sql file for future downloads

---
+++ dentaltorreon.sql
@@ -0,0 +1,329 @@
+-- phpMyAdmin SQL Dump
+-- version 4.7.0
+-- https://www.phpmyadmin.net/
+--
+-- Servidor: localhost
+-- Tiempo de generación: 11-07-2017 a las 03:47:07
+-- Versión del servidor: 10.1.24-MariaDB
+-- Versión de PHP: 7.0.20
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de datos: `dentaltorreon`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `cita`
+--
+
+CREATE TABLE `cita` (
+  `id_Cita` int(11) NOT NULL,
+  `fecha` date NOT NULL,
+  `id_persona` int(11) NOT NULL,
+  `id_medico` int(11) NOT NULL,
+  `id_TipoDeServicio` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `expediente`
+--
+
+CREATE TABLE `expediente` (
+  `id_Expediente` int(11) NOT NULL,
+  `id_persona` int(11) NOT NULL,
+  `ingreso` varchar(200) NOT NULL,
+  `comentarios` varchar(200) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `medico`
+--
+
+CREATE TABLE `medico` (
+  `id_medico` int(11) NOT NULL,
+  `nombre` varchar(50) NOT NULL,
+  `apellidoP` varchar(50) NOT NULL,
+  `apellidoM` varchar(50) NOT NULL,
+  `telefono` varchar(10) NOT NULL,
+  `especialidad` varchar(50) NOT NULL,
+  `cedula` varchar(15) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `persona`
+--
+
+CREATE TABLE `persona` (
+  `id_persona` int(11) NOT NULL,
+  `nombre` varchar(50) NOT NULL,
+  `apellidoP` varchar(50) NOT NULL,
+  `apellidoM` varchar(50) NOT NULL,
+  `direccion` varchar(100) NOT NULL,
+  `telefono` varchar(10) NOT NULL,
+  `edad` varchar(2) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `provedor`
+--
+
+CREATE TABLE `provedor` (
+  `id_Provedor` int(11) NOT NULL,
+  `representante` varchar(50) NOT NULL,
+  `direccion` varchar(100) NOT NULL,
+  `marca` varchar(50) NOT NULL,
+  `mercancia` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `receta`
+--
+
+CREATE TABLE `receta` (
+  `id_Receta` int(11) NOT NULL,
+  `fecha` varchar(50) NOT NULL,
+  `id_persona` int(11) NOT NULL,
+  `id_medico` int(11) NOT NULL,
+  `nombre_medicamento` varchar(50) NOT NULL,
+  `observaciones` varchar(50) NOT NULL,
+  `dosis` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `servicio`
+--
+
+CREATE TABLE `servicio` (
+  `id_servicio` int(11) NOT NULL,
+  `id_persona` int(11) NOT NULL,
+  `id_medico` int(11) NOT NULL,
+  `id_TipoDeServicio` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `tipodeservicio`
+--
+
+CREATE TABLE `tipodeservicio` (
+  `id_TipoDeServicio` int(11) NOT NULL,
+  `nombre_servicio` varchar(200) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `tratamiento`
+--
+
+CREATE TABLE `tratamiento` (
+  `id_tratamiento` int(11) NOT NULL,
+  `id_TipoDeServicio` int(11) NOT NULL,
+  `nombre_tratamiento` varchar(150) NOT NULL,
+  `comentarios` varchar(200) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `usuarios`
+--
+
+CREATE TABLE `usuarios` (
+  `user` varchar(20) NOT NULL,
+  `password` varchar(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Volcado de datos para la tabla `usuarios`
+--
+
+INSERT INTO `usuarios` (`user`, `password`) VALUES
+('admin', 'admin'),
+('medico1', 'medico1');
+
+--
+-- Índices para tablas volcadas
+--
+
+--
+-- Indices de la tabla `cita`
+--
+ALTER TABLE `cita`
+  ADD PRIMARY KEY (`id_Cita`),
+  ADD KEY `id_persona` (`id_persona`,`id_medico`,`id_TipoDeServicio`),
+  ADD KEY `id_medico` (`id_medico`),
+  ADD KEY `id_TipoDeServicio` (`id_TipoDeServicio`);
+
+--
+-- Indices de la tabla `expediente`
+--
+ALTER TABLE `expediente`
+  ADD PRIMARY KEY (`id_Expediente`),
+  ADD KEY `id_persona` (`id_persona`);
+
+--
+-- Indices de la tabla `medico`
+--
+ALTER TABLE `medico`
+  ADD PRIMARY KEY (`id_medico`);
+
+--
+-- Indices de la tabla `persona`
+--
+ALTER TABLE `persona`
+  ADD PRIMARY KEY (`id_persona`);
+
+--
+-- Indices de la tabla `provedor`
+--
+ALTER TABLE `provedor`
+  ADD PRIMARY KEY (`id_Provedor`);
+
+--
+-- Indices de la tabla `receta`
+--
+ALTER TABLE `receta`
+  ADD PRIMARY KEY (`id_Receta`),
+  ADD KEY `id_persona` (`id_persona`,`id_medico`),
+  ADD KEY `id_medico` (`id_medico`);
+
+--
+-- Indices de la tabla `servicio`
+--
+ALTER TABLE `servicio`
+  ADD PRIMARY KEY (`id_servicio`),
+  ADD KEY `id_persona` (`id_persona`,`id_medico`,`id_TipoDeServicio`),
+  ADD KEY `id_TipoDeServicio` (`id_TipoDeServicio`),
+  ADD KEY `id_medico` (`id_medico`);
+
+--
+-- Indices de la tabla `tipodeservicio`
+--
+ALTER TABLE `tipodeservicio`
+  ADD PRIMARY KEY (`id_TipoDeServicio`);
+
+--
+-- Indices de la tabla `tratamiento`
+--
+ALTER TABLE `tratamiento`
+  ADD PRIMARY KEY (`id_tratamiento`),
+  ADD KEY `id_TipoDeServicio` (`id_TipoDeServicio`);
+
+--
+-- AUTO_INCREMENT de las tablas volcadas
+--
+
+--
+-- AUTO_INCREMENT de la tabla `cita`
+--
+ALTER TABLE `cita`
+  MODIFY `id_Cita` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT de la tabla `expediente`
+--
+ALTER TABLE `expediente`
+  MODIFY `id_Expediente` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT de la tabla `medico`
+--
+ALTER TABLE `medico`
+  MODIFY `id_medico` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT de la tabla `persona`
+--
+ALTER TABLE `persona`
+  MODIFY `id_persona` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT de la tabla `provedor`
+--
+ALTER TABLE `provedor`
+  MODIFY `id_Provedor` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT de la tabla `receta`
+--
+ALTER TABLE `receta`
+  MODIFY `id_Receta` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT de la tabla `servicio`
+--
+ALTER TABLE `servicio`
+  MODIFY `id_servicio` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT de la tabla `tipodeservicio`
+--
+ALTER TABLE `tipodeservicio`
+  MODIFY `id_TipoDeServicio` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT de la tabla `tratamiento`
+--
+ALTER TABLE `tratamiento`
+  MODIFY `id_tratamiento` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- Restricciones para tablas volcadas
+--
+
+--
+-- Filtros para la tabla `cita`
+--
+ALTER TABLE `cita`
+  ADD CONSTRAINT `cita_ibfk_1` FOREIGN KEY (`id_medico`) REFERENCES `medico` (`id_medico`),
+  ADD CONSTRAINT `cita_ibfk_2` FOREIGN KEY (`id_persona`) REFERENCES `persona` (`id_persona`),
+  ADD CONSTRAINT `cita_ibfk_3` FOREIGN KEY (`id_TipoDeServicio`) REFERENCES `tipodeservicio` (`id_TipoDeServicio`);
+
+--
+-- Filtros para la tabla `expediente`
+--
+ALTER TABLE `expediente`
+  ADD CONSTRAINT `expediente_ibfk_1` FOREIGN KEY (`id_persona`) REFERENCES `persona` (`id_persona`);
+
+--
+-- Filtros para la tabla `receta`
+--
+ALTER TABLE `receta`
+  ADD CONSTRAINT `receta_ibfk_1` FOREIGN KEY (`id_persona`) REFERENCES `persona` (`id_persona`),
+  ADD CONSTRAINT `receta_ibfk_2` FOREIGN KEY (`id_medico`) REFERENCES `medico` (`id_medico`);
+
+--
+-- Filtros para la tabla `servicio`
+--
+ALTER TABLE `servicio`
+  ADD CONSTRAINT `servicio_ibfk_1` FOREIGN KEY (`id_persona`) REFERENCES `persona` (`id_persona`),
+  ADD CONSTRAINT `servicio_ibfk_2` FOREIGN KEY (`id_TipoDeServicio`) REFERENCES `tipodeservicio` (`id_TipoDeServicio`),
+  ADD CONSTRAINT `servicio_ibfk_3` FOREIGN KEY (`id_medico`) REFERENCES `medico` (`id_medico`);
+
+--
+-- Filtros para la tabla `tratamiento`
+--
+ALTER TABLE `tratamiento`
+  ADD CONSTRAINT `tratamiento_ibfk_1` FOREIGN KEY (`id_TipoDeServicio`) REFERENCES `tipodeservicio` (`id_TipoDeServicio`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
