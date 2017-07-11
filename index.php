e521b0dd0a8bb24283bfb9e561ed92a6902d24ff
hitler2006 <hitler1506@gmail.com>
2017-07-11 01:19:01 -0500

    Creo que ya pude
    ok

--- index.php
+++ index.php
@@ -4,6 +4,7 @@
         <link rel="stylesheet" type="text/css" href="bootstrap/css/bootstrap.min.css">
         <script src="bootstrap/query/jquery.min.js"></script>
         <script src="bootstrap/js/bootstrap.min.js"></script>
+        <link rel="stylesheet" type="text/css" href="estilopagina.css">
     </head>
     <body>
         <center><h1 id="Titulo">DENTAL TORREÓN </h1></center>
@@ -19,36 +20,6 @@
             </fieldset>
         </div>
         </center>
-        <style>
-        body{
-        background: url(img/log1.JPG);
-        }
-        #div2{
-        width: 1000px;
-        height: 40px;
-        }
-        #Titulo{
-        color: white;
-        font-family: Baskerrille Old Face;
-        font-size: 62px;
-        width: 593px;
-        height: 83px;
-        }
-        #input[type=submit]:hover {
-        cursor: pointer;
-        background: #000040;
-        color: white;
-        }
-        #feel{
-        width: 200px;
-        height: 135px;
-        background: #88C4FF;
-        }
-        #diente{
-        background: url('img/diente.png') no-repeat;
-        height: 555px;
-        width: 375px;
-        }
-        </style>
+        
     </body>
 </html>
